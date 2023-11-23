ff2098923c0b5cae961cbdf21a2c5dc453ded20f
Smrity11 <kanisasia376@gmail.com>
2023-11-23 12:07:29 -0800

    set up firebase for this project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import AuthProvider from './provider/AuthProvider.jsx';
+import { router } from './routes/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <AuthProvider>
+      {/* <QueryClientProvider client={queryClient}> */}
+        {/* <HelmetProvider> */}
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        {/* </HelmetProvider> */}
+      {/* </QueryClientProvider> */}
+    </AuthProvider>
   </React.StrictMode>,
 )
